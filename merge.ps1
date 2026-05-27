de1da105223f5540fbc333ef2710a42bef098223
huavinhd <116966818+huavinhd@users.noreply.github.com>
2026-05-27 18:45:26 +0700

    Clean up mistakenly pushed untracked files and add .gitignore

--- merge.ps1
+++
@@ -1,5 +0,0 @@
-$main = Get-Content "SQL script\migrate_menu_REC_20260526.sql" -Raw -Encoding UTF8
-$procs = Get-Content "SQL script\migrate_menu_REC_20260526_Procedures2.sql" -Raw -Encoding UTF8
-$regex = "(?s)-- \[PHASE 1\] PROCEDURE DDLS.*?-- ----------------------------------------------------------------------------"
-$main = $main -replace $regex, $procs
-Set-Content "SQL script\migrate_menu_REC_20260526_Full.sql" -Value $main -Encoding UTF8
